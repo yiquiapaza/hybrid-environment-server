01c76b52a1529eeeb97b63bdb85107be2fa375f0
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-04-25 22:37:12 -0300

    Add websocket

--- index.js
+++ index.js
@@ -1,64 +1,25 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
-var bodyParser = require('body-parser');
-
-var fs = require('fs');
-
-var coutryData = JSON.parse(fs.readFileSync('newData.json'));
-
-var router = express.Router();
-
-var data = { country_code: 0 };
-var year = { year: 1950 };
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const originalUrl = (req, res, next) => {
-	console.log('Request URL: ', req.originalUrl);
-	next();
-};
-
-router.get('/', [originalUrl], (req, res) => {
-	res.send('Hello wordl');
+//const coutryData = JSON.parse(fs.readFileSync('newData.json'));
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+	cors: {
+		origin: 'http://127.0.0.1:9000',
+		methods: ['GET', 'POST'],
+		credentials: true,
+	},
 });
 
-router.post('/axis', [originalUrl], (req, res) => {
-	let data = req.body;
-
-	res.status(200).json({ message: 'ok' });
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world</h1>');
 });
 
-router.get('/data', [originalUrl], (req, res) => {
-	res.send(data);
+io.on('connection', (socket) => {
+	console.log(socket.id);
 });
 
-router.post('/data', [originalUrl], (req, res) => {
-	data = req.body;
-	console.log(data);
-	res.status(200).end();
+server.listen(3000, () => {
+	console.log('Server listening on Port: 3000');
 });
-
-router.get('/year', [originalUrl], (req, res) => {
-	res.send(year);
-});
-
-router.get('/year/:name', [originalUrl], (req, res) => {
-	var name = req.params.name;
-	var out = coutryData.filter(
-		(country) => country.name === name && country.year === year.year
-	);
-	res.send(out[0]);
-});
-
-router.post('/year', [originalUrl], (req, res) => {
-	year = req.body;
-	console.log(year);
-	res.status(200).end();
-});
-
-app.use(router);
-
-app.listen(3000);
