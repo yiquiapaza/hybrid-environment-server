774a71302afa9880eac2978760c2c4e97f88821d
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-08 01:18:09 -0300

    refactor and add rawdata

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-//const coutryData = JSON.parse(fs.readFileSync('newData.json'));
+const fs = require('fs');
+const raw_data = JSON.parse(fs.readFileSync('raw_data.json'));
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-	cors: {
-		origin: 'http://127.0.0.1:9000',
-		methods: ['GET', 'POST'],
-		credentials: true,
-	},
+
+const router = express.Router();
+
+router.get('/', (_, res) => {
+	console.log(raw_data);
+	res.send(raw_data);
 });
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello world</h1>');
-});
-
-io.on('connection', (socket) => {
-	console.log(socket.id);
-});
-
-server.listen(3000, () => {
-	console.log('Server listening on Port: 3000');
-});
+app.use(router);
+app.listen(3000);
