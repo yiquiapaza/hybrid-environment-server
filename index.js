477fc9955547b44599e2ff8d5f0f719f4ce0fdb5
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-14 00:10:50 -0300

    Update post method

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const express = require('express');
 const fs = require('fs');
 const raw_data = JSON.parse(fs.readFileSync('raw_data.json'));
-const express = require('express');
-const cors = require('cors');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-
 const router = express.Router();
+
+app.use(express.json());
+
+let country = { country: '', status: 0 };
 
 router.get('/', (_, res) => {
 	console.log(raw_data);
 	res.send(raw_data);
+});
+
+router.post('/', (req, res) => {
+	country = req.body;
+	res.status(200);
 });
 
 app.use(router);
