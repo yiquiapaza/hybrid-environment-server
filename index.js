a59d741d4cf22e489a1cd50e3510344f68e0af80
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-22 00:36:25 -0300

    Add state router

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const raw_data = JSON.parse(fs.readFileSync('raw_data.json'));
 const app = express();
 const router = express.Router();
+const cors = require('cors')
 
+app.use(cors());
 app.use(express.json());
 
 let country = { name: '', status: 0 };
+let state = {state: 0};
 
 router.get('/', (_, res) => {
 	console.log(raw_data);
@@ -21,6 +24,15 @@ router.post('/country', (req, res) => {
 	country = req.body;
 	res.status(200).send('Success');
 });
+
+router.get('/state', (_, res)=> {
+	res.status(200).send(state);
+})
+
+router.post('/state', (req, res) => {
+	state = res.body;
+	res.status(200).send('Success')
+})
 
 app.use(router);
 app.listen(3000);
