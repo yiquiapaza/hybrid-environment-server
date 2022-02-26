1c482539de643e33f9aa8abf4e9fac7c08ade0a8
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-26 00:46:20 -0300

    Refactor index

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+// core dep
 import express from 'express';
 import fs from 'fs';
 
 const raw_data = JSON.parse(fs.readFileSync('raw_data.json'));
+
+// express config
 const app = express();
 const router = express.Router();
-
 app.use(express.json());
 app.use(express.urlencoded());
 
-//$ npm install express --save
 
-let country = { name: '', status: 0 };
+// global
+let country = { name: '', state: 0 };
 let rotation = { x: 0, y: 0, z: 0 };
 
 // TODO:
 // Create GET and POST position service URL
-// 	Create GET and POST rotation service URL
-// 	Create a hook for interaction or maybe just a new URL for GET and POST information about elements
-//  Create log for eeach event
+// Create GET and POST rotation service URL
+// Create a hook for interaction or maybe just a new URL for GET and POST information about elements
+// Create log for eeach event
 
 router.get('/', (_, res) => {
 	console.log(raw_data);
