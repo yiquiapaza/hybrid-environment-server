ca3b57b4defef5c0ba8d4c8d842fc3a41c975066
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-11-02 23:55:56 -0300

    feat: add routers

---
+++ routers/barchart.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/barchart', () =>{});
+
+router.post('/barchart/desktop', () => {});
+
+router.post('/barchart/hololens', () => {});
+
+module.exports = router;
